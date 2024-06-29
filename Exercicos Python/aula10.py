002a09243065ef1fa72ca6e51e08c13087ebd879
Guilherme Hermano <guilherme.hermano1902@gmail.com>
2024-06-28 21:29:28 -0300

    Delete aula10.py

--- Exercicos Python/aula10.py
+++
@@ -1,9 +0,0 @@
-print('digite 4 numerdos de 0 a 100')
-n1=int(input('Digite um numer:'))
-n2=int(input('Digite outro:'))
-n3=int(input('Digite o terceiro:'))
-n4=int(input('digite'))
-n10= n1 % 2
-n20= n2 % 2
-n30= n3 % 2
-n40= n4 % 2
