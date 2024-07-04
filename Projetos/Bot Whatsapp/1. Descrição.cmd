4b8f13777008875de212d8a92d7e68870861d923
Guilherme Hermano <guilherme.hermano1902@gmail.com>
2024-07-03 23:04:17 -0300

    descriçao

---
+++ Projetos/Bot Whatsapp/1. Descrição.cmd
@@ -0,0 +1,3 @@
+Projeto feito para aprendizado, enviando mensagens automatizadas pelo whatsapp utilizando uma planilha no exel.
+Feito pelo canal do youtube 'Dev Aprender | Jhonatan de Souza' 
+https://www.youtube.com/watch?v=gv9dRvkn0rI
